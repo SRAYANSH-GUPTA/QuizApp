54098f2792c807763b2e8f50bfaa7129140b8760
SRAYANSH-GUPTA <sr.gupta621@gmail.com>
2025-01-19 20:50:20 +0530

    Created Project

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:quiz_app/screens/question_screen.dart';
+
+import 'package:quiz_app/screens/start_screen.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const StartScreen(),
+    );
+  }
+}
